b0ec3ab770e7a2843b328e8b7b051b75087509fc
andriesvanveen-source <andriesvanveen@gmail.com>
2026-09-01 07:03:00 +0200

    Add iPhone home screen icon and web app manifest

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,22 @@ import SessionRouteRestore from "./components/SessionRouteRestore";
 
 export const metadata = {
   title: "Hull Master",
-  description: "Boat defect logging by discipline"
+  description: "Boat defect logging by discipline",
+  manifest: "/manifest.webmanifest",
+  applicationName: "Hull Master",
+  appleWebApp: {
+    capable: true,
+    title: "Hull Master",
+    statusBarStyle: "black-translucent"
+  },
+  icons: {
+    icon: [{ url: "/robertson-caine-logo-transparent.png", type: "image/png" }],
+    apple: [{ url: "/robertson-caine-logo-transparent.png", type: "image/png" }]
+  }
+};
+
+export const viewport = {
+  themeColor: "#071b2e"
 };
 
 export default function RootLayout({ children }) {
