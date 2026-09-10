aaf68e7515ab2c3785c2af4593913d17675e693a
andriesvanveen-source <andriesvanveen@gmail.com>
2026-09-10 14:10:25 +0200

    Enable zooming on mobile quality audits

---
+++ app/quality-control/boats/[boatId]/layout.js
@@ -0,0 +1,11 @@
+export const viewport = {
+  width: 1100,
+  minimumScale: 0.25,
+  maximumScale: 5,
+  userScalable: true,
+  themeColor: "#071b2e"
+};
+
+export default function QualityBoatLayout({ children }) {
+  return children;
+}
